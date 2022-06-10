eacf8327be1adf32b9d01fbaf973ba55d6e707bd
angkasaID <yogasapputra123@gmail.com>
2022-06-10 11:00:14 -0600

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,20 @@
 module.exports = {
   content: ["index.html"],
   theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
     extend: {
       colors: {
         primary: "#22d3ee",
         dark: "#0f172a",
-      }
+      },
+
+    screens: {
+      "2xl" : "1320px",
+    },
+
     },
   },
   plugins: [],
